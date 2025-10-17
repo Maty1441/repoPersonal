7ea78179576a784693be834615c2d4f9a743e574
Maty1441 <mathiasls610@gmail.com>
2025-10-17 19:35:06 -0300

    ejercicio 5 listo

--- DiseñoWeb/Semestre2/Semana9-11/ej5/js/js.js
+++ DiseñoWeb/Semestre2/Semana9-11/ej5/js/js.js
@@ -1,22 +1,35 @@
-$("#agregarYmostrar").click(agregarYmostrar);
+$("#enviar").click(tomarDatos);
 
-let usuarios = [];
+let requeridas = ["1234 ABC", "5678 DEF", "9101 GHI", "1121 JKL", "3141 MNO", "5161 PQR", "7181 STU", "9202 VWX", "1222 YZA", "3242 BCD"];
 
-function agregarYmostrar() {
-    let valor = $("#valor").val();
+function tomarDatos() {
+    let matricula = $("#matricula").val();
+    matricula = (matricula || "").toUpperCase();
 
-    usuarios.push(valor);
-
-    console.log("Usuario agregado");
-    console.log(usuarios);
-
-    $("#mensaje").html("");
-    $("#mensaje").html(mostrarUsuarios());
-
+    verificarMatricula(matricula);
 }
 
-function mostrarUsuarios() {
-    for(i=0; i<usuarios.length; i++) {
-        $("#mensaje").append(usuarios[i] + "<br>");
+function verificarMatricula(matricula) {
+    if (!matricula || String(matricula).length < 8) {
+        console.log('Valor no válido');
+         $("#mensaje").html("MATRICULA NO VALIDA");
+    }else if (requeridas.includes(matricula)) {
+        console.log('MATRICULA REQUERIDA');
+        $("#mensaje").html('<p.mensaje="mensaje" style="color: red;">MATRICULA REQUERIDA</p>');
+    } else {
+        $("#mensaje").html('<p.mensaje="mensaje" style="color: green;">MATRICULA NO REQUERIDA</p>');
+    }
+}
+
+function mostrarMensaje(mensaje) {
+    if (mensaje === "MATRICULA REQUERIDA") {
+        $("#mensaje").html("");
+        $("#mensaje").html('<p class="mensaje" style="color: green;">' + mensaje + '</p>');
+    } else if (mensaje === "MATRICULA NO REQUERIDA") {
+        $("#mensaje").html("");
+        $("#mensaje").html('<p class="mensaje" style="color: red;">' + mensaje + '</p>');
+    } else {
+        $("#mensaje").html("");
+        $("#mensaje").html('<p class="mensaje" style="color: orange;">' + mensaje + '</p>');
     }
 }
